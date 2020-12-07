2d7cfe7639f1aa98f2c586951abe732665c098bd
ksolomon7 <kalkidan.solomon2@gmail.com>
2020-12-07 12:57:40 -0500

    adds client request

--- demo_http.js
+++ demo_http.js
@@ -8,10 +8,20 @@ var http= require('http')
 //http has to be defined and the http module has to be required
 
 
-//how the response should be displayed
+//how the response should be displayed and status code
+
+// http.createServer(function(req,res){
+//     res.writeHead(200, {'Content-Type': 'text/html'});
+//     res.write("Hello World");
+//     res.end();
+// }).listen(8080)
+
+// request from the client, the object has a property called
+//url which holds the part that comes after the domain name
 
 http.createServer(function(req,res){
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write("Hello World");
+    res.write(req.url);
+    console.log("this is req", req.url)
     res.end();
-}).listen(8080)
+}).listen(8080);
