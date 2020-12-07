ff18f1234a343dda86c775e2e5596be9802c0d3e
ksolomon7 <kalkidan.solomon2@gmail.com>
2020-12-07 12:08:55 -0500

    adds writeHead for status code

--- demo_http.js
+++ demo_http.js
@@ -1,6 +1,17 @@
 var http= require('http')
 
+// http.createServer(function(req,res){
+//     res.write("Hello World"); //response to the client
+//     res.end()// end of response
+// }).listen(8080) //set to the port
+
+//http has to be defined and the http module has to be required
+
+
+//how the response should be displayed
+
 http.createServer(function(req,res){
+    res.writeHead(200, {'Content-Type': 'text/html'});
     res.write("Hello World");
-    res.end()
+    res.end();
 }).listen(8080)
