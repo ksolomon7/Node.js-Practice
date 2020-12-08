c938c3747a709eaa19bf82a5f8688d29677c9eff
ksolomon7 <kalkidan.solomon2@gmail.com>
2020-12-07 20:49:49 -0500

    adds query string

--- demo_http.js
+++ demo_http.js
@@ -1,4 +1,5 @@
 var http= require('http')
+var url= require('url')
 
 // http.createServer(function(req,res){
 //     res.write("Hello World"); //response to the client
@@ -19,9 +20,19 @@ var http= require('http')
 // request from the client, the object has a property called
 //url which holds the part that comes after the domain name
 
+// http.createServer(function(req,res){
+//     res.writeHead(200, {'Content-Type': 'text/html'});
+//     res.write(req.url);
+//     console.log("this is req", req.url)
+//     res.end();
+// }).listen(8080);
+
+// splitting query string
 http.createServer(function(req,res){
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(req.url);
-    console.log("this is req", req.url)
-    res.end();
-}).listen(8080);
+    res.writeHead(200,{"Content-Type": "text/html"})
+    var q=url.parse(req.url,true).query;
+    //q is an object in order to pull out the values you need 
+    //to do q.year
+    var txt= q.year +" "+ q.month;
+    res.end(txt)
+}).listen(8080)
